a0b489d6b74d4e37eabfe8e892acaa3547d583fa
minh Duc <minhduc08022004@gmail.com>
2026-04-19 10:50:55 +0700

    feat: update data api with correct types and GET methods

---
+++ src/api/data.api.ts
@@ -0,0 +1,19 @@
+import type { UserType } from "../type/user";
+import type { MatchParticipant } from "../type/match-history";
+import type { Transaction } from "../type/transaction";
+import axiosClient from "./axiosClient";
+import { API_ENDPOINTS } from "./endpoints";
+
+export const dataApi = {
+  profile: () =>
+    axiosClient.get<UserType>(API_ENDPOINTS.USER.PROFILE)
+    .then(res=>res.data),
+
+  transactions: () =>
+    axiosClient.get<Transaction[]>(API_ENDPOINTS.USER.TRANSACTIONS)
+    .then(res=>res.data),
+
+  matches: () =>
+    axiosClient.get<MatchParticipant[]>(API_ENDPOINTS.USER.MATCHES)
+    .then(res=>res.data),
+};
